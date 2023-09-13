e199be1a511fae1cc5f65eb4a8f86fa07bc3316f
aymen saidani <aymensaidany@gmail.com>
2023-09-13 17:52:23 +0200

    done

---
+++ src/data/data.js
@@ -0,0 +1,29 @@
+
+
+export const data = [
+  {
+    id: 1,
+    img: 'https://i.postimg.cc/qBKLpn6W/ecom.png',
+    title: "Phone Store (NextJs)",
+    desc: "This is an e-commerce web site built using Next JS, CSS, Materiel UI, Node js , Express for database we use the orm Sequalize. It includes search functionality, The application features user authentication and enables users to create, edit, and delete their own posts or profile. as well as the ability to add and remove items from a shopping cart.",
+    link: "",
+    git: "https://github.com/aymensaidani/ecommerceNext",
+  },
+  {
+    id: 2,
+    img: 'https://i.postimg.cc/vTzBz3D4/screencapture-localhost-3000-shop-2023-09-11-19-10-11.png',
+    title: "Cosmetic Store (ReactJs)",
+    desc: "This is an e-commerce web site built using React JS, CSS, Bootstrap, Node js , Express and for database we use the Mysql . It includes search functionality",
+    link: "",
+    git: "https://github.com/aymensaidani/RBKTN-02-23-Mvp-Starter",
+  },
+  {
+    id: 4,
+    img: 'https://i.postimg.cc/L5LSS6yq/Pr-sentation-commerciale-Lancement-de-Projet-Aspiration-et-l-gance-en-Noir-et-Blanc-meraude-et-Ver.png',
+    title: "Mobile Application (React Native) ",
+    desc:  "This is an mobile application built using React NativeJ, CSS, Google map, Node js , Express and for database Sequalize . It includes search functionality  by name or categorie ,The application features user authentication and enables users to create, edit, and delete their own posts or profile, as well as view posts created by others. as well as the ability for payment with stripe",
+    link: "",
+    git: "https://github.com/WamiaService/Wamia",
+  }
+  
+];

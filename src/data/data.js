4dd3afa4c395d505177a446bd0978fbf9cbb67ec
aymen saidani <aymensaidany@gmail.com>
2023-09-18 16:54:12 +0200

    add certeficate

--- src/data/data.js
+++ src/data/data.js
@@ -5,7 +5,7 @@ export const data = [
     id: 1,
     img: 'https://i.postimg.cc/qBKLpn6W/ecom.png',
     title: "Phone Store (NextJs)",
-    desc: "This is an e-commerce web site built using Next JS, CSS, Materiel UI, Node js , Express for database we use the orm Sequalize. It includes search functionality, The application features user authentication and enables users to create, edit, and delete their own posts or profile. as well as the ability to add and remove items from a shopping cart.",
+    desc: "This is an e-commerce web site built using Next JS, CSS, Materiel UI, Node js , Express for database we use the orm Sequalize. It includes search functionality.",
     link: "",
     git: "https://github.com/aymensaidani/ecommerceNext",
   },
@@ -21,9 +21,19 @@ export const data = [
     id: 4,
     img: 'https://i.postimg.cc/L5LSS6yq/Pr-sentation-commerciale-Lancement-de-Projet-Aspiration-et-l-gance-en-Noir-et-Blanc-meraude-et-Ver.png',
     title: "Mobile Application (React Native) ",
-    desc:  "This is an mobile application built using React NativeJ, CSS, Google map, Node js , Express and for database Sequalize . It includes search functionality  by name or categorie ,The application features user authentication and enables users to create, edit, and delete their own posts or profile, as well as view posts created by others. as well as the ability for payment with stripe",
+    desc:  "This is an mobile application built using React Native, CSS, Google map, Node js , Express and for database Sequalize and payment with stripe",
     link: "",
     git: "https://github.com/WamiaService/Wamia",
-  }
+  },
+  {
+    id: 5,
+    img: 'https://i.postimg.cc/wM1zSfwH/E-Commerce-Facebook-Ad.png',
+    title: "FripoShop E-commerce Website (NextJS)",
+    desc:  "This is an mobile application built using NextJs, Tailwind CSS, firebase Node js , Express and for database Mongoose ODM and payment with strapi",
+    link: "",
+    git: "https://github.com/aymensaidani/friposhop.git",
+  },
+  
+
   
 ];
